Alch3myOS/packages_services_AxQuickLook

WeatherProvider: Update import path for OmniJawsClient
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/axion/quicklook/provider/WeatherProvider.kt
@@ -29,7 +29,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import androidx.core.graphics.drawable.toBitmap
-import com.android.internal.util.android.OmniJawsClient
+import com.android.internal.util.lunaris.OmniJawsClient
 import java.io.ByteArrayOutputStream
 import com.android.axion.quicklook.QuickLookAction
 import com.android.axion.quicklook.QuickLookTarget

Lineage-OS-Special-Project/android_packages_services_QuickLook

src/com/android/axion/quicklook/provider/WeatherProvider.kt
@@ -29,7 +29,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import androidx.core.graphics.drawable.toBitmap
-import com.android.internal.util.android.OmniJawsClient
+import com.android.internal.util.crdroid.OmniJawsClient
 import java.io.ByteArrayOutputStream
 import com.android.axion.quicklook.QuickLookAction
 import com.android.axion.quicklook.QuickLookTarget